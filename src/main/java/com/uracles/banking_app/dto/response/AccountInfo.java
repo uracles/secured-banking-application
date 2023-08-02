8363361e98a34faf5cdf79aec5667cdea1574d26
Miracle Shaibu <120692848+uracles@users.noreply.github.com>
2023-08-02 01:19:49 +0100

    data transfer objects

---
+++ src/main/java/com/uracles/banking_app/dto/response/AccountInfo.java
@@ -0,0 +1,18 @@
+package com.uracles.banking_app.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountInfo {
+    private String accountName;
+
+    private String accountBalance;
+
+    private String accountNumber;
+}
