ed3a88d1c5ceed3f52f72b8596175dbe75efa555
Miracle Shaibu <120692848+uracles@users.noreply.github.com>
2023-08-05 23:44:45 +0100

    account changes

--- src/main/java/com/uracles/banking_app/dto/response/AccountInfo.java
+++ src/main/java/com/uracles/banking_app/dto/response/AccountInfo.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -12,7 +14,7 @@ import lombok.NoArgsConstructor;
 public class AccountInfo {
     private String accountName;
 
-    private String accountBalance;
+    private BigDecimal accountBalance;
 
     private String accountNumber;
 }
