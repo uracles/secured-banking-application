c961d8e532281b5f2a3f4968ff3b5ddc2566976b
Miracle Shaibu <120692848+uracles@users.noreply.github.com>
2023-08-31 23:46:54 +0100

    email dto

--- src/main/java/com/uracles/banking_app/dto/request/EmailDetails.java
+++ src/main/java/com/uracles/banking_app/dto/request/EmailDetails.java
@@ -1,10 +1,11 @@
 package com.uracles.banking_app.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @AllArgsConstructor @NoArgsConstructor
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
 public class EmailDetails {
     private String recipient;
     private String messageBody;
