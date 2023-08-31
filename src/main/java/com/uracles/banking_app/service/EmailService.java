e21ecb9d1b650bdb9ea283a92ca1c72cb70d77b8
Miracle Shaibu <120692848+uracles@users.noreply.github.com>
2023-08-31 23:48:35 +0100

    email service

--- src/main/java/com/uracles/banking_app/service/EmailService.java
+++ src/main/java/com/uracles/banking_app/service/EmailService.java
@@ -1,7 +1,9 @@
 package com.uracles.banking_app.service;
 
 import com.uracles.banking_app.dto.request.EmailDetails;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface EmailService {
     void sendEmailAlert(EmailDetails emailDetails);
 }
