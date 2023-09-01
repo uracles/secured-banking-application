70f85456baa99e8f2729f23d02d3877fe594c12e
Miracle Shaibu <120692848+uracles@users.noreply.github.com>
2023-09-01 08:56:29 +0100

    updated services implementations

--- src/main/java/com/uracles/banking_app/service/implementation/AppUserServiceImpl.java
+++ src/main/java/com/uracles/banking_app/service/implementation/AppUserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.uracles.banking_app.service.implementation;
 
 import com.uracles.banking_app.dto.request.AppUserRequestDto;
+import com.uracles.banking_app.dto.request.EmailDetails;
 import com.uracles.banking_app.dto.response.AccountInfo;
 import com.uracles.banking_app.dto.response.BankResponse;
 import com.uracles.banking_app.entity.AppUser;
 import com.uracles.banking_app.repository.AppUserRepository;
 import com.uracles.banking_app.service.AppUserService;
+import com.uracles.banking_app.service.EmailService;
 import com.uracles.banking_app.utils.AccountUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,8 @@ public class AppUserServiceImpl implements AppUserService {
 
 
     private final AppUserRepository appUserRepository;
+
+    private final EmailService emailService;
     @Override
     public BankResponse createAccount(AppUserRequestDto userRequest) {
         //check if user already has an account
@@ -49,7 +53,14 @@ public class AppUserServiceImpl implements AppUserService {
                     .build();
 
             AppUser savedAppUser = appUserRepository.save(newUser);
-
+            //send email alert
+            EmailDetails emailDetails =EmailDetails.builder()
+                    .recipient(savedAppUser.getEmail())
+                    .subject("ACCOUNT CREATION")
+                    .messageBody("Congratulations, Your Account Has Been Successfully Created. \nYour Account Details: \n" +
+                            "Account Name: " + savedAppUser.getFirstName() + " " + savedAppUser.getLastName() + " " + savedAppUser.getOtherName() + "\n Account Number: " + savedAppUser.getAccountNumber())
+                    .build();
+            emailService.sendEmailAlert(emailDetails);
 
             return BankResponse.builder()
                     .responseCode(AccountUtils.ACCOUNT_CREATION_SUCCESS_CODE)
--- src/main/java/com/uracles/banking_app/service/implementation/EmailServiceImpl.java
+++ src/main/java/com/uracles/banking_app/service/implementation/EmailServiceImpl.java
@@ -1,4 +1,42 @@
 package com.uracles.banking_app.service.implementation;
 
-public class EmailServiceImpl {
+import com.uracles.banking_app.dto.request.EmailDetails;
+import com.uracles.banking_app.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+@Async
+public class EmailServiceImpl implements EmailService{
+
+
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+
+    @Override
+    public void sendEmailAlert(EmailDetails emailDetails) {
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(senderEmail);
+            mailMessage.setTo(emailDetails.getRecipient());
+            mailMessage.setText(emailDetails.getMessageBody());
+            mailMessage.setSubject(emailDetails.getSubject());
+
+            javaMailSender.send(mailMessage);
+            System.out.println("Mail Sent Successfully");
+        } catch (MailException e){
+            throw new RuntimeException(e);
+        }
+    }
 }
