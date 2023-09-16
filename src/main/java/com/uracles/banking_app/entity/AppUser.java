9859ee7e0ff15af009c141e4ed97abf97fc3f7ab
Uracles <miracle.shaibu@deacon.dev>
2023-09-16 13:04:55 +0100

    new

--- src/main/java/com/uracles/banking_app/entity/AppUser.java
+++ src/main/java/com/uracles/banking_app/entity/AppUser.java
@@ -1,10 +1,10 @@
 package com.uracles.banking_app.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
